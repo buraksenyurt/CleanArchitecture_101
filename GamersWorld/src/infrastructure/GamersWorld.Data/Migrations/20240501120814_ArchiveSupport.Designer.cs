56d1c39f9a73c47c6fa08020a3bbc5061caa15ba
buraksenyurt <burakselimsenyurt@gmail.com>
2024-05-01 15:21:02 +0300

    Migrated with new column

--- GamersWorld/src/infrastructure/GamersWorld.Data/Migrations/20240501120814_ArchiveSupport.Designer.cs
+++ GamersWorld/src/infrastructure/GamersWorld.Data/Migrations/20240501120814_ArchiveSupport.Designer.cs
@@ -2,6 +2,7 @@
 using GamersWorld.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GamersWorld.Data.Migrations
 {
     [DbContext(typeof(GamersWorldDbContext))]
-    partial class GamersWorldDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240501120814_ArchiveSupport")]
+    partial class ArchiveSupport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
---
+++ GamersWorld/src/infrastructure/GamersWorld.Data/Migrations/20240501120814_ArchiveSupport.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamersWorld.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArchiveSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "Games",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "Games");
+        }
+    }
+}
